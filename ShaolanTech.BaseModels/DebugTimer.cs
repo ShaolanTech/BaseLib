471e095b5ca8412f5ea2800996633ed65e78d8c6
warensoft <warensoft@163.com>
2022-11-28 15:06:29 +0800

    init

---
+++ ShaolanTech.BaseModels/DebugTimer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Warensoft.EntLib.Common
+{
+    public class DebugTimer : IDisposable
+    {
+        public DebugTimer(bool enable = true)
+        {
+            this.enable = enable;
+        }
+        DateTime time = DateTime.Now;
+        private bool enable;
+        public bool UseConsole { get; set; } = false;
+        private void WriteLine(string message)
+        {
+            if (this.UseConsole)
+            {
+                Console.WriteLine(message);
+            }
+            else
+            {
+                Debug.WriteLine(message);
+            }
+        }
+        public void CheckSpan(string title = "",bool reset=false)
+        {
+            if (this.enable)
+            {
+                WriteLine($"{title}:{(DateTime.Now - this.time).TotalMilliseconds}");
+            }
+            if (reset)
+            {
+                this.Reset();
+            }
+        }
+        public void Reset()
+        {
+            this.time = DateTime.Now;
+        }
+        public void Dispose()
+        {
+            if (this.enable)
+            {
+                //Debug.WriteLine($"finished time span:{(DateTime.Now - this.time).TotalMilliseconds}");
+
+            }
+        }
+    }
+}
---
+++ ShaolanTech.BaseModels/ResultInfo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ShaolanTech.BaseModels
+{
+
+    /// <summary>
+    /// Result container for invoking a method or an api, this type is used when this method or api has no return value
+    /// </summary>
+    public class ResultInfo
+    {
+
+        private bool operationDone;
+
+        /// <summary>
+        /// Whether this call is successful or not
+        /// </summary>
+        public bool OperationDone
+        {
+            get { return operationDone; }
+            set
+            {
+                operationDone = value;
+                if (!value)
+                {
+                    this.LogTime = DateTime.Now;
+
+                }
+            }
+        }
+
+        public DateTime LogTime { get; set; }
+
+        private string message;
+
+        /// <summary>
+        /// Message when this call is failed
+        /// </summary>
+        public string Message
+        {
+            get { return message; }
+            set
+            {
+                
+                message = value;
+
+            }
+        }
+
+        public int Code { get; set; }
+
+        public ResultInfo()
+        {
+            this.OperationDone = true;
+            this.LogTime = DateTime.Now;
+            this.message = "";
+        } 
+    }
+    /// <summary>
+    /// Result container for invoking a method or an api, this type is used when this method or api has return value, and you need fill "AdditionalData" property before returning.
+    /// </summary>
+    public class ResultInfo<T> : ResultInfo
+    {
+
+        public T AdditionalData { get; set; }
+    }
+
+}
